6da76c02681630350e8c84a20b99b03f520b4b9b
git9527 <zhangshaoneng@etradeunion.com>
2020-09-26 13:33:34 +0800

    use Referer when Origin is empty

--- pkg/util/utils.go
+++ pkg/util/utils.go
@@ -7,6 +7,15 @@ import (
 
 func GetOriginHost(c *gin.Context) string {
 	origin := c.Request.Header.Get("Origin")
-	index := strings.Index(origin, "//")
-	return origin[index+2:]
+	refer := c.Request.Header.Get("Referer")
+	if origin != "" {
+		index := strings.Index(origin, "//")
+		return origin[index+2:]
+	} else {
+		index := strings.Index(refer, "//")
+		subRefer := refer[index+2:]
+		index2 := strings.Index(subRefer, "/")
+		return subRefer[0:index2]
+	}
+
 }
